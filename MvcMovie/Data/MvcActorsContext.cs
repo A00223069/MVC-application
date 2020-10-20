b3ec332336cbc7ee283629c51adcca4ee1ef7739
Bharti Rishi <66094357+A00223069@users.noreply.github.com>
2020-10-19 23:25:01 -0400

    MVC Application

---
+++ MvcMovie/Data/MvcActorsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
+
+namespace MvcMovie.Data
+{
+    public class MvcActorsContext : DbContext
+    {
+        public MvcActorsContext(DbContextOptions<MvcActorsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Actors> Actors { get; set; }
+    }
+}
